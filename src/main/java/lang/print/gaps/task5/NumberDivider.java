332076c1ec968b646e348a7f3fcdc730e46625d0
tikmart <t.martirosyan012@gmail.com>
2022-07-12 16:16:53 +0400

    Task 5 first commit

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.task5;
 
+import java.util.Scanner;
+
 public class NumberDivider {
     void divide(int toBeDivided) {
+
+        toBeDivided = 19;
+
+        toBeDivided /=5;
+
+        System.out.println(toBeDivided);
+
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        minutes = 11.5F;
+        minutes *= 60;
+
+        System.out.println(minutes);
     }
 }
